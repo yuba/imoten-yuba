70f802b2769a399e9100bdc51ee53558893c302a
shoozhoo@gmail.com <shoozhoo@gmail.com@d9e7f8b0-fdcf-11de-94bc-491a2344f988>
2010-11-13 06:27:26 +0000

    verのみ更新 (1.1.21)
    git-svn-id: http://imoten.googlecode.com/svn/trunk@140 d9e7f8b0-fdcf-11de-94bc-491a2344f988

--- immf/src/immf/ServerMain.java
+++ immf/src/immf/ServerMain.java
@@ -38,7 +38,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.cookie.Cookie;
 
 public class ServerMain {
-	public static final String Version = "imoten (imode.net mail tenson) ver. 1.1.20";
+	public static final String Version = "imoten (imode.net mail tenson) ver. 1.1.21";
 	private static final Log log = LogFactory.getLog(ServerMain.class);
 
 	private ImodeNetClient client;
@@ -111,7 +111,7 @@ public class ServerMain {
 
 		// 転送抑止ドメインリスト読み込み
 		this.loadIgnoreDomainList();
-		
+
 		try{
 			// 前回のcookie
 			if(this.conf.isSaveCookie()){
